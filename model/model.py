114734881e64dc21cca98bde4668868bc0aa9043
romith05 <117058379+romith05@users.noreply.github.com>
2025-08-09 19:18:30 -0600

    Update model.py

--- model/model.py
+++ model/model.py
@@ -9,10 +9,14 @@ from concurrent.futures import ThreadPoolExecutor
 app = Flask(__name__)
 
 # Load model once at startup
-with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
-with open("knn_model.pkl", "rb") as f:
-    knn = pickle.load(f)
+with open("/data/rf_model.pkl", "rb") as rf:
+    rfm = pickle.load(rf)
+with open("/data/my_forest_encoder.pkl","rb") as rfle:
+    rfm_label_encoder = pickle.load(rfle)
+with open("/data/knn_model.pkl", "rb") as knn:
+    knnm = pickle.load(knn)
+with open("/data/knn_encoder.pkl", "rb") as knnle:
+    knn_label_encoder = pickle.load(knnle)
 
 executor = ThreadPoolExecutor(max_workers=2)
 
@@ -44,7 +48,7 @@ def fetch_weather(lat, lon):
         return None
 
 def knn_predict(lat, lon):
-    return knn.predict([[lat, lon]])[0]
+    return knnm.predict([[lat, lon]])[0]
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -62,16 +66,23 @@ def predict():
     # Wait for results
     weather = future_weather.result()
     vegetation_type = future_vegtype.result()
+    vegetation_label = knn_label_encoder.inverse_transform([vegetation_type])[0]
 
     if weather is None:
-        return jsonify({"error": "Weather data unavailable"}), 503
+        #return jsonify({"error": "Weather data unavailable"}), 503
+        weather = { "temperature": 10, "relative_humidity": 0, "wind_speed": 100}
+    
+    # Prepare features for prediction   
+    if vegetation_type is None:
+        return jsonify({"error": "Vegetation type prediction failed"}), 503
+    X = np.array([[weather["temperature"], weather["relative_humidity"], weather["wind_speed"], rfm_label_encoder.transform([vegetation_label])[0]]])
 
-    X = np.array([[weather["temperature"], weather["relative_humidity"], weather["wind_speed"], vegetation_type]])
-    prob = model.predict_proba(X)[0][1]
+    prob = rfm.predict_proba(X)[0][1]
 
     return jsonify({
         "probability": round(prob, 3),
-        "weather": weather
+        "weather": weather,
+        "vegetation_type": vegetation_label
     })
 
 if __name__ == "__main__":
